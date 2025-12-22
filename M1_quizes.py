5f792e2eca3a52a622428babe93a1f6b2975b3ab
Aerotipo <marcos.da.perez@gmail.com>
2025-12-22 20:29:00 +0000

    updated with quiz 4

---
+++ M1_quizes.py
@@ -0,0 +1,17 @@
+import numpy as np 
+from math import sin, cos, tan, atan, pi, sqrt
+
+#===================================
+#quiz 4 - Conic Section Properties
+#===================================
+"""a = 8000  #km
+e = 0.23  #eccentricity (ellipse)
+f = 295*pi/180 # true anomaly angle
+rp = a*(1-e)
+ra = a*(1+e)
+p = rp*(1+e)
+b = a*sqrt(1-e**2)
+E = 2*atan(sqrt((1-e)/(1+e))*tan(f/2))*180/pi
+
+print(a,e,f,rp,ra,p,b,E+360)"""
+
