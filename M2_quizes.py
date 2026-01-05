4e2b6df3acaca94ff6cf9c76cf5ac7a78d950f28
Aerotipo <marcos.da.perez@gmail.com>
2026-01-05 12:04:49 +0000

    uncommented for final commit

--- M2_quizes.py
+++ M2_quizes.py
@@ -7,56 +7,56 @@ from math import sin, cos, tan, atan, pi, sqrt
 #===================================
 # exercise 4
 #===================================
-# k1 = 3
-# k3 = 1
-# w = sqrt(k1)
+k1 = 3
+k3 = 1
+w = sqrt(k1)
 
-# #Time domain
-# dt = 0.0001
-# t = np.arange(0,10+dt,dt)
+#Time domain
+dt = 0.0001
+t = np.arange(0,10+dt,dt)
 
-# #Initial conditions
-# e1 = 1
-# e2 = 0
+#Initial conditions
+e1 = 1
+e2 = 0
 
-# for ti in t:
-#     x = e1*cos(w*ti) + e2/w*sin(w*ti)
-#     ad = -k3*x**3
-#     e1_dot = -(1/w*sin(w*ti))*ad
-#     e2_dot = cos(w*ti)*ad
-#     e1 += e1_dot*dt
-#     e2 += e2_dot*dt
+for ti in t:
+    x = e1*cos(w*ti) + e2/w*sin(w*ti)
+    ad = -k3*x**3
+    e1_dot = -(1/w*sin(w*ti))*ad
+    e2_dot = cos(w*ti)*ad
+    e1 += e1_dot*dt
+    e2 += e2_dot*dt
 
-# print("e1 at", t[-1], "s = ", e1)
-# print("e2 at", t[-1], "s = ", e2)
+print("e1 at", t[-1], "s = ", e1)
+print("e2 at", t[-1], "s = ", e2)
 
-# #===================================
-# #quiz 2 - Lagrangian Brackets
-# #===================================
-# # exercise 2
-# #===================================
-# k = 3       #spring constant
-# g = 9.81    #m/s, gravity acceleration
-# w = sqrt(k) #natural frequency of the "imagined" spring-mass system
+#===================================
+#quiz 2 - Lagrangian Brackets
+#===================================
+# exercise 2
+#===================================
+k = 3       #spring constant
+g = 9.81    #m/s, gravity acceleration
+w = sqrt(k) #natural frequency of the "imagined" spring-mass system
 
-# #Time domain
-# dt = 0.0001
-# t = np.arange(0,10+dt,dt)
+#Time domain
+dt = 0.0001
+t = np.arange(0,10+dt,dt)
 
-# #Initial conditions
-# e1 = 1
-# e2 = 0
+#Initial conditions
+e1 = 1
+e2 = 0
 
-# for ti in t:
-#     x = e1*cos(w*ti) + e2/w*sin(w*ti)
-#     ad = -g
-#     e1_dot = -(1/w*sin(w*ti))*ad
-#     e2_dot = cos(w*ti)*ad
-#     e1 += e1_dot*dt
-#     e2 += e2_dot*dt
+for ti in t:
+    x = e1*cos(w*ti) + e2/w*sin(w*ti)
+    ad = -g
+    e1_dot = -(1/w*sin(w*ti))*ad
+    e2_dot = cos(w*ti)*ad
+    e1 += e1_dot*dt
+    e2 += e2_dot*dt
 
-# print("e1 at", t[-1], "s = ", e1)
-# print("e2 at", t[-1], "s = ", e2)
+print("e1 at", t[-1], "s = ", e1)
+print("e2 at", t[-1], "s = ", e2)
 
 #===================================
 #quiz 4 - Poisson Brackets
@@ -86,3 +86,4 @@ for ti in t:
 
 print("e1 at", t[-1], "s = ", e1)
 print("e2 at", t[-1], "s = ", e2)
+
